37fba32da22aa4f01fb260a92c2e9f88fe5183a1
nkoeninger@finley <nicokoeninger@arcor.de>
2022-06-20 12:03:25 +0200

    Added Event Function

--- src/main/java/de/dhbw/woped/process2text/dataModel/bpmnReader/BPMNReader.java
+++ src/main/java/de/dhbw/woped/process2text/dataModel/bpmnReader/BPMNReader.java
@@ -33,4 +33,55 @@ public class BPMNReader {
 
     }
 
+    private static void extractEvents(Document doc, ProcessModel model, Pool pool, Lane lane) {
+        NodeList start_event = doc.getElementsByTagName("bpmn:startEvent");
+        NodeList end_event = doc.getElementsByTagName("bpmn:endEvent");
+        int newId;
+
+        for (int i = 0; i < start_event.getLength(); i++) {
+            Node fstNode = start_event.item(i);
+            newId = model.getNewId();
+            model.addEvent(new Event(newId, "", lane, pool, EventType.START_EVENT));
+        }
+
+        for (int i = 0; i < end_event.getLength(); i++) {
+            Node fstNode = end_event.item(i);
+            newId = model.getNewId();
+            model.addEvent(new Event(newId, "", lane, pool, EventType.END_EVENT));
+        }
+
+
+
+
+    }
+    private static void extractGateways(Document doc, ProcessModel model, Pool pool, Lane lane){
+
+        /*
+        To-Do:
+            - Analysieren, ob im BPMN Format noch weitere Gateway-Operatoren? (Inklusives OR)
+            - Pool und Lane des jeweiligen Gateways bestimmen --> Aufruf aus Pool und Lane Methode
+        */
+
+        NodeList list_AND = doc.getElementsByTagName("bpmn:parallelGateway");
+        NodeList list_XOR = doc.getElementsByTagName("bpmn:exclusiveGateway");
+        //NodeList list_OR;
+        int newId;
+
+        for (int i = 0; i < list_AND.getLength(); i++) {
+            Node fstNode = list_AND.item(i);
+            newId = model.getNewId();
+            model.addGateway(new Gateway(newId, "", lane, pool, GatewayType.AND));
+        }
+
+        for (int i = 0; i < list_XOR.getLength(); i++) {
+            Node scdNode = list_AND.item(i);
+            newId = model.getNewId();
+            model.addGateway(new Gateway(newId, "", lane, pool, GatewayType.XOR));
+        }
+
+
+    }
+
+
+
 }
