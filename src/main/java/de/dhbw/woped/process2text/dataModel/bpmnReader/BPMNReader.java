395f315978426bc1452fa277bc30c7f9e13d7f47
delugas14 <delukass@web.de>
2022-06-22 09:11:30 +0200

    Added extractActivity and extractArc

--- src/main/java/de/dhbw/woped/process2text/dataModel/bpmnReader/BPMNReader.java
+++ src/main/java/de/dhbw/woped/process2text/dataModel/bpmnReader/BPMNReader.java
@@ -11,6 +11,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Map;
 
 public class BPMNReader {
     public ProcessModel getProcessModelFromBPMNString(InputStream input){
@@ -31,6 +32,29 @@ public class BPMNReader {
     }
     private static void extractFlow(Document doc, ProcessModel model){
 
+    }
+
+    private static void extractActivity(Document doc, ProcessModel model, String id, Pool pool, Lane lane){
+        //Liste aller Activities
+        NodeList list = doc.getElementById(pool.getBPMNId()).getChildNodes();
+        String label;
+        int type = 1;
+        NodeList participants = doc.getElementsByTagName("bpmn:Participant");
+        for (int i = 0; i < participants.getLength(); i++) {
+            Element participant = (Element) participants.item(i);
+            if (participant.getAttribute("processRef") == pool.getBPMNId()) {
+                if (participant.getAttribute("name") == "Company" || participant.getAttribute("name") == "User") {
+                    type = 2;
+                }
+            }
+        }
+        for (int i = 0; i < list.getLength(); i++) {
+            Element frstNode = (Element) list.item(i);
+            if (frstNode.getTagName() == "bpmn:task") {
+                model.addActivity(new Activity(model.getNewId(), frstNode.getAttribute("name"), lane, pool, type));
+            }
+
+        }
     }
 
     private static void extractEvents(Document doc, ProcessModel model, Pool pool, Lane lane) {
@@ -81,6 +105,55 @@ public class BPMNReader {
         }
 
 
+    }
+
+    private static void extractArc(Document doc, ProcessModel model) {
+        NodeList list = doc.getElementsByTagName("bpmn:sequenceFlow");
+        for (int i = 0; i < list.getLength(); i++) {
+            Node fstNode = list.item(i);
+            Element arc = (Element) fstNode;
+            String sourceRef = arc.getAttribute("sourceRef");
+            String targetRef = arc.getAttribute("targetRef");
+            String label = arc.getAttribute("id");
+            de.dhbw.woped.process2text.dataModel.process.Element source = null;
+            de.dhbw.woped.process2text.dataModel.process.Element target = null;
+            HashMap<Integer, Activity> activities = model.getActivites();
+            HashMap<Integer, Event> events = model.getEvents();
+            HashMap<Integer, Gateway> gateways = model.getGateways();
+            for (Map.Entry e : activities.entrySet()) {
+                Activity activity = (Activity) e.getValue();
+                if (activity.getLabel() == sourceRef) {
+                    source = activity;
+                }
+                if (activity.getLabel() == targetRef) {
+                    target = activity;
+                }
+            }
+            if (source.equals(null) || target.equals(null)) {
+                for (Map.Entry e : events.entrySet()) {
+                    Event event = (Event) e.getValue();
+                    if (event.getLabel() == sourceRef) {
+                        source = event;
+                    }
+                    if (event.getLabel() == targetRef) {
+                        target = event;
+                    }
+                }
+                if (source.equals(null) || target.equals(null)) {
+                    for (Map.Entry e : gateways.entrySet()) {
+                        Gateway gateway = (Gateway) e.getValue();
+                        if (gateway.getLabel() == sourceRef) {
+                            source = gateway;
+                        }
+                        if (gateway.getLabel() == targetRef) {
+                            target = gateway;
+                        }
+                    }
+                }
+
+                model.addArc(new Arc(model.getNewId(), label, source, target));
+            }
+        }
     }
 
 
